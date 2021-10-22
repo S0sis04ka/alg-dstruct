7114bb172af4ad8af452afaeb6e6e3916a6b9137
Son Artyom <sai@modumlab.com>
2021-10-22 23:19:59 +0300

    added stress-test

--- 2-Son-C2/Lab-C-2.c
+++ 2-Son-C2/Lab-C-2.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define MAXN (int)1e7 + 1
+/*
+  Chip:	Apple M1
+  Total Number of Cores:	8 (4 performance and 4 efficiency)
+  RAM:	8 GB
+  SSD: 256 GB
+
+  Stress_test results:
+  Time: 44.825529 sec on graph with 80000 vertex
+  Memory: 23,8 Gb
+ */
 
 int n;
 int** tree;
-int was[MAXN];
-int queue[MAXN];
+int* was;
+int* queue;
 int count = 0;
 
 void dfs(int node) {
@@ -21,6 +31,24 @@ void dfs(int node) {
     }
 };
 
+void stress_test() {
+    unsigned long start = clock();
+    int num_v = 80000;
+    free(tree);
+    tree = (int**)malloc(sizeof(int*) * num_v); //32000
+    queue = (int*) malloc(sizeof(int) * num_v);
+    was = (int*) malloc(sizeof(int) * num_v); //640000 byte
+    for (int i = 0; i < num_v; i++) {
+        tree[i] = (int*) malloc(sizeof(int) * num_v);
+        for (int j = 0; j < num_v; j++) {
+            tree[i][j] = rand() % 2;
+        }
+    }
+    dfs(0);
+    double time = (double)(clock() - start)/CLOCKS_PER_SEC;
+    printf("%f\n", time);
+}
+
 int main() {
     fscanf(stdin, "%d", &n);
 
@@ -31,6 +59,8 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         tree[i] = (int*)malloc(sizeof(int) * n);
+        was = (int*)malloc(sizeof(int) * n);
+        queue = (int*)malloc(sizeof(int) * n);
         if (tree[i] == NULL) {
             return 1;
         }
@@ -47,6 +77,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         fprintf(stdout, "%d ", queue[i]);
     }
+
+//    stress_test();
 
     return 0;
 }
