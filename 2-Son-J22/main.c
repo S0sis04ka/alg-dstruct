fe96946924593c97f025d1553dde545d26ad3e49
Son Artyom <sai@modumlab.com>
2022-04-24 07:11:29 +0300

    fixed tabs

--- 2-Son-J22/main.c
+++ 2-Son-J22/main.c
@@ -26,15 +26,15 @@ int hash(const char *str, int iter) {
 	int c0 = 31;
 	double c1 = 0.5;
 	double c2 = 0.5;
-    unsigned int h1 = 0;
-    for (int i = 0 ; str[i] != '\0' ; i++)
-    {
-        h1 = c1 * h1 + str[i];
-    }
+	int h1 = 0;
+	for (int i = 0 ; str[i] != '\0' ; i++)
+	{
+		h1 = c0 * h1 + str[i];
+	}
 	h1 = h1 % size;
 
 	int h2 = (int)(h1 + c1*iter + c2*iter*iter) % size;
-	
+
 	return h2;
 }
 
@@ -104,25 +104,24 @@ int main() {
 	char command;
 	char value [10000];
 	while (scanf("%c",&command) > 0) {
-        if (command == 'a') {
-            scanf("%s", &value);
-            Insert(value);
-        }
-        if (command == 'f') {
-            scanf("%s", &value);
-		if (Search(value))
-			printf("%s", "yes\n");
-		else 
-			printf("%s", "no\n");
-        }
-        if (command == 'r'){
-            scanf("%s", &value);
-            Delete(value);
-        }
-    }
+		if (command == 'a') {
+			scanf("%s", &value);
+			Insert(value);
+		}
+		if (command == 'f') {
+			scanf("%s", &value);
+			if (Search(value))
+				printf("%s", "yes\n");
+			else 
+				printf("%s", "no\n");
+		}
+		if (command == 'r'){
+			scanf("%s", &value);
+			Delete(value);
+		}
+	}
 	DestroyHashMap();
 
 	return 0;
 }
-
 
